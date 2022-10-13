49c4a0c0bdc4968dea6ffa84e4f7cad01fc5d0f4
MaximAgarev <maximagarev@yandex.ru>
2022-10-13 23:52:14 +0400

    Задача спринта 3, первое ревью

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var counterValue: Int = 0 {
+        didSet {
+            if counterValue == 100000 {
+                counterLabel.font = UIFont.systemFont(ofSize:64.0)
+            }
+            counterLabel.text = "\(counterValue)"
+        }
+    }
 
-
+    @IBAction func counterDidTap(_ sender: Any) {
+        counterValue += 1
+    }
+    
+    @IBAction func resetDidTap(_ sender: Any) {
+        counterValue = 0
+    }
 }
+
 
